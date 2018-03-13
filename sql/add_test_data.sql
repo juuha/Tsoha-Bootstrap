045de275106bd6e57fc9c4ee965060b462badd54
Juuha <juha.ritakoski@hotmail.com>
2018-03-13 12:25:27 +0200

    sql taulut ja testi-data luotu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,47 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+--Author-taulun testidata
+INSERT INTO Author (name, password) VALUES ('J', 'eihyväsalis');
+
+
+--Story-taulun testidata
+INSERT INTO Story (name, author_id, genre, date_of_addition, synopsis) VALUES
+('Maze', 1, 'Mysteeri', '08.08.2088', 'You wake up in a strange room, with no memory
+	on how or why you are there. You only see 4 doors around you and a compass at the
+	center of the room. You feel a sense of urgency to get out, something is coming.');
+
+
+--Scene-taulun testidata
+INSERT INTO Scene (name, story_id, question, situation, first_scene) VALUES
+('Waking Up', 1, 'What are you going to do?', 'You wake up in a weird 
+	room with 4 doors, 1 on each of the walls surrounding you. 
+	You see a compass stuck on the floor in the middle of the room. 
+	It points north towards the door in front of you. You feel a 
+	sense of urgency, but aren''t sure why.', 1);
+
+INSERT INTO Scene (name, story_id, question, situation)  VALUES 
+('Door on the Left', 1, 'What will you do?', 'You enter the room and
+	see a small wooden crate. It has a skull drawn onto it with, what you
+	deem to be, blood. You see a crowbar chained to the ground next to 
+	the crate');
+
+INSERT INTO Scene (name, story_id, question, situation) VALUES 
+('What''s in the crate (smash)', 1, 'You died from the burns', 'You grip the crowbar
+in your hands and slam the crate as hard as you can. You didn''t expect it, 
+but the crate exploded with shrapnels made of wood and glass, and splashes of 
+glowing liquid. The strange liquid covers you almost entirely and you feel burning
+pain. You pass out out from the pain');
+
+INSERT INTO Scene (name, story_id, question, situation) VALUES 
+('What''s in the crate (carefully)', 1, 'What do you want to do?', 'You creak open
+	the crate and find a glowing orb. It fills the room with bright green light. You
+	feel strange power from the orb.');
+
+
+--SceneLink-taulun testidata
+Insert INTO SceneLink (option_name, parent_scene_id, child_scene_id) VALUES 
+('Go through the left door.', 1, 2);
+
+INSERT INTO SceneLink (option_name, parent_scene_id, child_scene_id) VALUES 
+('Smash the crate with the crowbar!', 2, 3);
+
+INSERT INTO SceneLink (option_name, parent_scene_id, child_scene_id) VALUES
+('Pry open the crate carefully with the crowbar.', 2, 4);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Author(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	password varchar(50) NOT NULL 
+);
+
+CREATE TABLE Story(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	author_id INTEGER REFERENCES Author(id),
+	genre varchar(50),
+	synopsis varchar(1000),
+	date_of_addition DATE NOT NULL
+);
+
+CREATE TABLE Scene(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	story_id INTEGER REFERENCES Story(id),
+	situation varchar(1000) NOT NULL, --for actual story
+	question varchar(250) NOT NULL, --question for choice
+	first_scene INTEGER DEFAULT 0
+);
+
+CREATE TABLE SceneLink(
+	id SERIAL PRIMARY KEY,
+	option_name varchar(50) NOT NULL, --for choosing next scene
+	parent_scene_id INTEGER REFERENCES Scene(id),
+	child_scene_id INTEGER REFERENCES Scene(id)
+);
